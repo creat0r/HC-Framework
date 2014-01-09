a368ec0e46fd8161bc41da9e9aae1b985be96310
robgolbeck <rob@handcoded.ca>
2014-01-09 14:02:02 -0800

    default page template

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			
+		<article class="post" id="post-<?php the_ID(); ?>">
+
+		<h1><?php the_title(); ?></h1>
+
+				<?php the_content(); ?>
+
+				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+
+			<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+
+		</article><!--/.post-->
+
+		<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
