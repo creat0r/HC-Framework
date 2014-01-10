7d4a9250f0bf1186e592cedc6a613a1ce0ea68a4
robgolbeck <rob@handcoded.ca>
2014-01-09 22:45:01 -0800

    updated

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 
 // Enqueue Scripts
-function add_skunkyfw_scripts() {
-	wp_register_script('skunkyfw_functions', get_template_directory_uri() . '/assets/js/functions.js', array('jquery'), 1.0, true);
-	wp_enqueue_script('skunkyfw_functions');
+function add_hcfw_scripts() {
+	wp_register_script('hcfw_functions', get_template_directory_uri() . '/assets/js/functions.js', array('jquery'), 1.0, true);
+	wp_enqueue_script('hcfw_functions');
 }
-add_action( 'wp_enqueue_scripts', 'add_skunkyfw_scripts' );  
+add_action( 'wp_enqueue_scripts', 'add_hcfw_scripts' );  
 
 
 // Register Post Thumbnails
