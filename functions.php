6f6822afbf3dd64d1462b383a05f28eabd979dcb
Russell Heimlich <kingkool68@gmail.com>
2014-01-10 22:20:07 -0500

    Lets move this to the same place where the rest of our scripts are enqueued.

--- functions.php
+++ functions.php
@@ -59,6 +59,8 @@ function add_hcfw_scripts() {
 	
 	wp_register_script('hcfw_functions', get_template_directory_uri() . '/assets/js/functions.js', array('jquery'), 1.0, true);
 	wp_enqueue_script('hcfw_functions');
+	
+	if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
 }
 add_action( 'wp_enqueue_scripts', 'add_hcfw_scripts' );  
 
--- header.php
+++ header.php
@@ -23,7 +23,6 @@
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
 	<?php wp_head(); ?>
 
