ce52d791e02ae73ba510585ec1ece78c8b607bf8
Russell Heimlich <kingkool68@gmail.com>
2014-01-10 23:41:22 -0500

    We can make an alias to the global jQuery object in the context of the ready() function. All this means is we can use the $ for jQuery safely since our code is contained from the rest of the JavaScript on the page. And writing $ is much easier than writing out jQuery.
    See "Aliasing the jQuery Namespace" http://api.jquery.com/ready/

--- functions.php
+++ functions.php
@@ -19,14 +19,14 @@ add_action( 'optionsframework_custom_scripts', 'optionsframework_custom_scripts'
 function optionsframework_custom_scripts() { ?>
 
 <script type="text/javascript">
-jQuery(document).ready(function() {
+jQuery(document).ready(function($) {
 
-	jQuery('#example_showhidden').click(function() {
-  		jQuery('#section-example_text_hidden').fadeToggle(400);
+	$('#example_showhidden').click(function() {
+  		$('#section-example_text_hidden').fadeToggle(400);
 	});
 
-	if (jQuery('#example_showhidden:checked').val() !== undefined) {
-		jQuery('#section-example_text_hidden').show();
+	if ($('#example_showhidden:checked').val() !== undefined) {
+		$('#section-example_text_hidden').show();
 	}
 
 });
